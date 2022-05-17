d28235876d969f7a27cb307015bf3a156c316ccf
Daniel2002927 <3585045923@qq.com>
2022-05-17 10:42:45 +0800

    主页面的样式修改

---
+++ src/components/Main.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-container id="app">
+    <el-aside width="200px">
+      <div>
+        <common-aside></common-aside>
+      </div>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <common-header></common-header>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </el-container>
+</template>
+
+
+
+<script>
+import commonAside from "./common/commonAside.vue";
+import commonHeader from "./common/commonHeader.vue"
+export default {
+  name: "Main",
+  components: {
+    commonAside,
+    commonHeader
+  },
+};
+</script>
+
+<style scoped>
+.el-header {
+  background-color: black;
+  /* he
+  ight: 0px; */
+}
+
+#app {
+  background-image: url(../assets/background.jpg);
+  background-size: contain;
+}
+</style>
