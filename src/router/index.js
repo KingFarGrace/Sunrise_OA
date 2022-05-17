129004280ccd1aeba76da58bd5020d8850f1dc37
Daniel2002927 <3585045923@qq.com>
2022-05-17 10:21:27 +0800

    Revert "Revert "Revert "主页面的样式的搭建"""
    This reverts commit 68dd4472b403f3f051a69ad71de6f692836ffcdf.

--- src/router/index.js
+++ src/router/index.js
@@ -1,69 +1,41 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
-import Main from '@/components/Main'
+import Home from '@/components/Home'
 import Register from '@/components/Register'
-import Home from '@/components/views/Home'
-import Group from '@/components/views/Group'
-import Seat from '@/components/views/Seat'
-import Resource from '@/components/views/Resource'
-import Git from '@/components/views/Git'
-import Personal from '@/components/Personal'
+
 
 Vue.use(Router)
 
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'Login',
+//       component: Login
+//     }
+//   ]
+// })
 
 const routes = [
   {
     path: '/',
-    name: 'Login',
-    component: Login
+      name: 'Login',
+      component: Login
   },
   {
-    path: '/main',
-    name: 'Main',
-    component: Main,
-    children:[
-      {
-        path:'/',
-        name:'Home',
-        component:Home
-      },
-      {
-        path:'/group',
-        name:'Group',
-        component:Group
-      },
-      {
-        path:'/seat',
-        name:'Seat',
-        component:Seat
-      },
-      {
-        path:'/resource',
-        name:'Resource',
-        component:Resource
-      },
-      {
-        path:'/git',
-        name:'Git',
-        component:Git
-      }
-    ]
+    path: '/home',
+      name: 'Home',
+      component: Home
   },
   {
     path: '/register',
-    name: 'Register',
-    component: Register
-  },
-  {
-    path: '/personal',
-    name: 'Personal',
-    component: Personal
+      name: 'Register',
+      component: Register
   }
 ]
 
-const router = new Router({
+const router  = new Router({
   routes
 })
 
